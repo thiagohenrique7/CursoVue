ffec9317884e70794117beeabf2354366d43b265
thiagohenrique7 <thiagohenriquean@gmail.com>
2022-03-16 22:21:50 -0300

    components

---
+++ vue-component-introducao/src/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <app-contador>
+
+    </app-contador>
+</template>
+
+<script>
+import appContador from './appContador.vue'
+
+export default {
+    components:{
+        'app-contador':appContador
+    }
+}
+</script>
---
+++ vue-component-introducao/src/appContador.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2> Clicado {{contador}} vezes </h2>
+        <button @click="incrementar"> Incrementar </button>
+    </div>
+</template>
+
+<script>
+//definindo o bloco script
+export default {
+    data(){
+        return{
+           contador: 0
+        }
+    },
+    methods:{
+        incrementar(){
+            this.contador++
+        }
+    }
+}
+</script>
